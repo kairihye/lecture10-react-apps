92044e03171d668115e89ba6f8ceb1b3a6147d60
kairi hye <hye@uw.edu>
2016-11-07 09:37:52 -0800

    add import bootstrap css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 //css files
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <App />,
